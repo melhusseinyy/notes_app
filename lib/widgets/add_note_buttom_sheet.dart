896116f5c6e2a2857da00c0666248b66c2adcc92
Mohamed El Husseiny <mohamedellhusseiinyy9@gmail.com>
2025-05-26 14:24:33 +0300

    date format

--- lib/widgets/add_note_buttom_sheet.dart
+++ lib/widgets/add_note_buttom_sheet.dart
@@ -1,12 +1,8 @@
-import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notesapp/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notesapp/widgets/add_note_form.dart';
-import 'package:notesapp/widgets/custom_button.dart';
-import 'package:notesapp/widgets/custom_text_field.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
   const AddNoteButtomSheet({super.key});
